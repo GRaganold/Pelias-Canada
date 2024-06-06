f2b8bc4993a6c470303c4274d6c8d821d9760cb5
Geordin Raganold <91495472+GRaganold@users.noreply.github.com>
2024-06-06 12:38:22 -0400

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('react-root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
